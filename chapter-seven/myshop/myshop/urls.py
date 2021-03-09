a3a3e25c2d2f611b632da79e7688ac59115a1ef2
BikoPython <stevennyandoya@gmail.com>
2021-03-09 12:17:36 +0300

    additions:celery-and-flower

--- chapter-seven/myshop/myshop/urls.py
+++ chapter-seven/myshop/myshop/urls.py
@@ -19,8 +19,9 @@ from django.conf.urls.static import static
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls), 
     path('cart/', include('cart.urls', namespace='cart')),
+    path('orders/', include('orders.urls', namespace='orders')),
     path('', include('shop.urls', namespace='shop')),
 ]
 
